7735d8cd71072dfe4c1c63d84ae9ea630a7b13c1
FallenHopes <parob2014@gmail.com>
2021-04-18 22:04:57 +0300

    Map

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { MainMap } from './map/MainMap'
 
 export const App = () => {
   return(
     <div>
       <h1 style={{ textAlign: 'center' }}>Ну здесь типо ваш дашборд</h1>
       <h4 style={{ textAlign: 'center' }}>Да начнутся голодные игры)))</h4>
+      <MainMap />
     </div>
   )
 }
